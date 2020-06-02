e94b5d6dbb0c1cede6157583b6a1a5c2ac08040a
natalia_vll <natalia_vll@mail.ru>
2020-06-02 11:55:12 +0300

    add parsing html

--- import_data_to_mongoDB.py
+++ import_data_to_mongoDB.py
@@ -1,9 +1,11 @@
+import lxml as lxml
 import pymongo
 import hashlib
 import os
 import connection
+from bs4 import BeautifulSoup
 
-def import_doc(path):
+def parsing_doc(path):
     f = open(path, 'r', encoding="utf-8")
     title = f.readline()
 
@@ -11,18 +13,31 @@ def import_doc(path):
     _line = doc.partition('Содержание статьи:\n')
     _l = _line[2].partition('\n\n')
     description = _l[0]
+    f.close()
 
+    import_doc(doc, title, description)
+
+
+def parsing_html(path):
+    with open(path, "r", encoding="utf-8") as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+        title = soup.title.text + '\n'
+        description = soup.aside.text
+        doc = title + '\n' + soup.main.text
+
+        import_doc(doc, title, description)
+
+
+def import_doc(doc, title, description):
     hash_object = hashlib.md5(doc.encode())
     hash = hash_object.hexdigest()
-
     try:
         mydict = {"_id": hash, "doc": doc, "title": title, "description": description}
         x = col.insert_one(mydict)
-        # export_md5_data(hash, doc)
     except Exception as e:
         print(e)
 
-    f.close()
 
 
 col = connection.connection_mongodb_texts()
@@ -31,5 +46,10 @@ directory = 'C://archive_storage_for_clustering'
 files = os.listdir(directory)
 
 for item in files:
+    name = item.partition('.')
+    doc_format = name[2]
     way = directory + "/" + item
-    import_doc(way)
+    if doc_format == 'html':
+        parsing_html(way)
+    if doc_format == 'txt':
+        parsing_doc(way)
